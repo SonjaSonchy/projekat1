76ccb38637482137e4f68473dc50d77f9d87e360
SonjaSonchy <47255256+SonjaSonchy@users.noreply.github.com>
2019-02-02 13:01:15 +0100

    Update sketch.js
    Interakcija sa tastaturom

--- sketch.js
+++ sketch.js
@@ -1,12 +1,15 @@
-var positionX = 0;
+var positionX = 50;
+var positionY = 50;
 
-var positionY = 0;
+var speedX=1;
+var speedY=0;
 
+var speedFactor=4;
 
 
 function setup() {
 
-  createCanvas(400, 300);
+ createCanvas(800, 600);
 
 }
 
@@ -14,33 +17,35 @@ function setup() {
 
 function draw() {
 
-  background(200);
+ background(200);
+
+ updatePosition();
+
+ nacrtajTelo(positionX, positionY);
+
+ nacrtajOci();
+
+ nacrtajKapu();
+
+ if (mouseIsPressed) {
+
+   nacrtajUsta(true);
+
+ } else {
+
+   nacrtajUsta(false);
+
+ }
+
+}
 
 
 
-  positionX = mouseX;
+function updatePosition(){
 
-  positionY = mouseY;
+ positionX = positionX+speedX*speedFactor;
 
-
-
-  nacrtajTelo(positionX, positionY);
-
-  nacrtajOci();
-
-  nacrtajKapu();
-
-
-
-  if (mouseIsPressed) {
-
-    nacrtajUsta(true);
-
-  } else {
-
-    nacrtajUsta(false);
-
-  }
+ positionY = positionY+speedY*speedFactor;
 
 }
 
@@ -48,11 +53,11 @@ function draw() {
 
 function nacrtajTelo(x, y) {
 
-  strokeWeight(4);
+ strokeWeight(4);
 
-  fill(0,170,0);
+ fill(0,170,0);
 
-  rect(x - 50, y - 50, 100, 100);
+ rect(x - 50, y - 50, 100, 100);
 
 }
 
@@ -60,17 +65,17 @@ function nacrtajTelo(x, y) {
 
 function nacrtajKapu() {
 
-  push();
+ push();
 
-  stroke(0);
+ stroke(0);
 
-  translate(positionX - 60, positionY - 60);
+ translate(positionX - 60, positionY - 60);
 
-  fill(255,100,30);
+ fill(255, 100, 30);
 
-  triangle(0, 0, 0, 60, 60, 0);
+ triangle(0, 0, 0, 60, 60, 0);
 
-  pop();
+ pop();
 
 }
 
@@ -78,11 +83,11 @@ function nacrtajKapu() {
 
 function nacrtajOci() {
 
-  fill(255);
+ fill(255);
 
-  ellipse(positionX - 50 + 25, positionY - 50 + 30, 20, 20)
+ ellipse(positionX - 50 + 25, positionY - 50 + 30, 20, 20)
 
-  ellipse(positionX - 50 + 70, positionY - 50 + 30, 30, 30)
+ ellipse(positionX - 50 + 70, positionY - 50 + 30, 30, 30)
 
 }
 
@@ -90,33 +95,64 @@ function nacrtajOci() {
 
 function nacrtajUsta(zatvorena) {
 
-  if (zatvorena) {
+ if (zatvorena) {
 
-    stroke(0);
+   stroke(0);
 
-    line(positionX - 30, positionY + 30, positionX + 30, positionY + 30)
+   line(positionX - 30, positionY + 30, positionX + 30, positionY + 30)
 
-  } else {
+ } else {
 
-    noStroke();
+   noStroke();
 
-    fill(255, 0, 0);
+   fill(255, 0, 0);
 
-    rect(positionX - 30, positionY + 13, 60, 20);
+   rect(positionX - 30, positionY + 13, 60, 20);
 
-  }
+ }
 
 
 
-  fill(255);
+ fill(255);
 
-  noStroke();
+ noStroke();
 
-  rect(positionX - 30, positionY + 18, 10, 10);
+ rect(positionX - 30, positionY + 18, 10, 10);
 
-  rect(positionX + 20, positionY + 18, 10, 10);
+ rect(positionX + 20, positionY + 18, 10, 10);
 
 }
 
 
 
+function keyPressed() {
+
+ if (keyCode === LEFT_ARROW) {
+
+    speedX=-1;
+
+    speedY=0;
+
+ }else if(keyCode === RIGHT_ARROW){
+
+    speedX=1;
+
+    speedY=0;
+
+ }else if(keyCode === UP_ARROW){
+
+    speedX=0;
+
+    speedY=-1;
+
+ }else if(keyCode === DOWN_ARROW){
+
+    speedX=0;
+
+    speedY=1;
+
+ }
+
+
+
+}
