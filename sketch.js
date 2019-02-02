0ff207da567ab3a36f75f7444c8e368237f0f918
SonjaSonchy <47255256+SonjaSonchy@users.noreply.github.com>
2019-02-02 12:08:17 +0100

    promenili kanvas
    Dodali smo mali kvadrat

--- sketch.js
+++ sketch.js
@@ -1,7 +1,19 @@
 function setup() {
-  createCanvas(400, 400);
-}
 
- function draw() {
-  background(0,50,100);
-}
+  createCanvas(400, 300);
+ 
+  background(200);
+ 
+ 
+ 
+  strokeWeight(4);
+ 
+ 
+ 
+  fill(0,170,0);
+ 
+  rect(width/2-50, height/2-50, 100, 100);
+ 
+ }
+
+ function draw () {};
