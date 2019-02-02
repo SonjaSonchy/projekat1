6bfd8eed9fadc06a2a426863ba1ee443ec30a5e5
SonjaSonchy <47255256+SonjaSonchy@users.noreply.github.com>
2019-02-02 12:32:04 +0100

    Update sketch.js
    dodali funkciju za mis

--- sketch.js
+++ sketch.js
@@ -2,33 +2,42 @@ function setup() {
 
   createCanvas(400, 300);
  
+ }
+ 
+ 
+ 
+ function draw() {
+ 
   background(200);
- 
- 
- 
-  strokeWeight(4);
- 
- 
- 
+
+  strokeWeight(2);
+
   fill(0,170,0);
-  rect(width/2-50, height/2-50, 100, 100);
+  rect(mouseX-50, mouseY-50, 100, 100);
 
-  fill(255);
-  ellipse(width/2-50+25, height/2-50+30, 20, 20);
-  ellipse(width/2-50+70, height/2-50+30, 30, 30);
 
-  line(width/2-30, height/2+30, width/2+30, height/2+30);
+
+  fill (255);
+  ellipse(mouseX-50+25, mouseY-50+30, 20, 20)
+  ellipse(mouseX-50+70, mouseY-50+30, 30, 30)
+
+
+
+  stroke(0);
+  line(mouseX-30, mouseY+30, mouseX+30, mouseY+30)
+
+
 
   noStroke();
-  rect(width/2-30, height/2+18, 10, 10);
-  rect(width/2+20, height/2+18, 10, 10);
+  rect(mouseX-30, mouseY+18, 10, 10);
+  rect(mouseX+20, mouseY+18, 10, 10);
 
-  // triangle(100, 250, 250, 170, 330, 300);
+
+
   stroke(0);
-  translate(width/2-60,height/2-60);
+  translate(mouseX-60,mouseY-60);
   fill(255,100,30);
   triangle(0, 0, 0, 60, 60, 0);
- 
+
  }
 
- function draw () {};
